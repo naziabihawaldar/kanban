fa8e1a035372ab0ba0fd4ec5ef82506c2cc2cbc6
laibaknock <120253464+laibaknock@users.noreply.github.com>
2023-11-20 16:00:31 +0530

    server added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: { 
+        hmr: {
+            host: '72.167.36.136' 
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
