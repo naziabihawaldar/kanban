25371a034beecad8093b59bcf417e3efd5eb5f97
laibaknock <120253464+laibaknock@users.noreply.github.com>
2023-12-28 16:32:48 +0530

    task scheduled

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('app:task-over-due')->dailyAt('00:05');
         // $schedule->command('inspire')->hourly();
     }
 
